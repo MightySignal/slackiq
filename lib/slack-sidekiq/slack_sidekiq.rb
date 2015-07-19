0747690a9d68c6a7fa4bb4b8fa35053be0d8cc5a
Jason Lew <jlew.project0@gmail.com>
2015-07-19 14:21:27 -0700

    cleanup

--- lib/slack-sidekiq/slack_sidekiq.rb
+++ lib/slack-sidekiq/slack_sidekiq.rb
@@ -15,38 +15,6 @@ class SlackSidekiq
     end
     
     def post
-      # text = {
-      #     "attachments" => [
-      #         {
-      #             "fallback" => "Required plain-text summary of the attachment.",
-      #
-      #             "color" => "#36a64f",
-      #
-      #             "pretext" => "Optional text that appears above the attachment block",
-      #
-      #             "author_name" => "Bobby Tables",
-      #             "author_link" => "http://flickr.com/bobby/",
-      #             "author_icon" => "http://flickr.com/icons/bobby.jpg",
-      #
-      #             "title" => "Slack API Documentation",
-      #             "title_link" => "https://api.slack.com/",
-      #
-      #             "text" => "Optional text that appears within the attachment",
-      #
-      #             "fields" => [
-      #                 {
-      #                     "title" => "Priority",
-      #                     "value" => "High",
-      #                     "short" => false
-      #                 }
-      #             ],
-      #
-      #             "image_url" => "http://my-website.com/path/to/image.jpg",
-      #             "thumb_url" => "http://example.com/path/to/thumb.png"
-      #         }
-      #     ]
-      # }.to_json
-      
       text = 'hello'
       
       attachments = 
